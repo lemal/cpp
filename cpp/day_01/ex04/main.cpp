49d7fada6c4ec48c12dd2b8538b192cbf41ad23e
Tayna Pulaski <tapulask@at-q2.msk.21-school.ru>
2022-12-01 16:41:30 +0300

    Separated into functions and corrected minor errors

--- cpp/day_01/ex04/main.cpp
+++ cpp/day_01/ex04/main.cpp
@@ -1,39 +1,28 @@
-#include "main.hpp"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tapulask <tapulask@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 15:28:17 by tapulask          #+#    #+#             */
+/*   Updated: 2022/12/01 16:22:04 by tapulask         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "replace.hpp"
 
 int main(int argc, char **argv)
 {
-  std::string   str;
-  std::string   needle(argv[2]);
-  std::string   fileName(argv[1]);
-  std::string   namePostfix(".replace");
-  std::ofstream outFile((fileName + namePostfix).c_str());
-  std::ifstream myFile(argv[1]);
-  std::size_t   loc;
-  
-  if (argc == 4 && myFile.fail())
-  {
-    std::cout << "No such file!" << std::endl;
-    return (0);
-  }  
-  if (argc != 4)
-  {
+    std::string     fileName(argv[1]);
+    std::string     namePostfix(".replace");
+    std::ifstream   myFile(argv[1]);
+
+    if (!inputValid(argc, myFile))
+        return (0);
+    std::ofstream outFile((fileName + namePostfix).c_str());
+    ft_replace(myFile, argv, outFile);
     myFile.close();
+    outFile.close();
     return (0);
-  }
-  while (true)
-  {
-    getline(myFile, str);
-    loc = str.find(argv[2]);
-    if (loc != std::string::npos)
-    {
-      str.erase(loc, needle.length());
-      str.insert(loc, argv[3]);
-    }
-    if (myFile.eof())
-      break;
-    outFile << str << std::endl;
-  }
-  myFile.close();
-  outFile.close();
-  return (0);
 }
--- cpp/day_01/ex04/main.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_HPP
-# define MAIN_HPP
-# include <string>
-# include <iostream>
-# include <fstream>
-#endif
